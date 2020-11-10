ed2b04d782f8592bd1af726a853416a02f7c29ec
Dmitry Shevchuk <dmitriy_shevchuk_01@ukr.net>
2020-11-10 20:44:37 +0200

    Style login page

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,22 @@ import LoginPage from './pages/Login'
 import MainPage from './pages/Main';
 import RegisterPage from './pages/Register'
 
+const Header = () => {
+  return (
+    <h1 style={{heigth: 100, width:'100%', textAlign: 'center', background: 'primary'}}>
+      <Switch>
+        <Route exact path="/">MAIN PA1GE</Route>
+        <Route path="/login">NO HOME PAGE</Route>
+      </Switch>
+    </h1>
+  )
+}
+
 function App() {
   const { isAuth } = useContext(AuthContext)
   return (
-    <div className="App">
+    <div className="App"> 
+      <Header />
       <Switch>
         <Route exact path="/" >
           { isAuth ? <MainPage /> : <HomePage /> }
@@ -24,5 +36,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,23 @@
 import React, { useContext, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { AuthContext } from '../contexts/Auth'
+import 'antd/dist/antd.css';
+import { Form, Input, Button, Checkbox } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+// import { StepForwardOutlined } from 'antd';
+
+
+
+// const layout = {
+//   labelCol: { span: 8 },
+//   wrapperCol: { span: 8 },
+//   textAlign: 'center'
+// };
+// const tailLayout = {
+//   wrapperCol: { offset: 8, span: 8 },
+//   textAlign: 'center'
+// };
+
 
 const LoginPage = () => {
   const auth = useContext(AuthContext)
@@ -28,12 +45,64 @@ const LoginPage = () => {
     <>      
       <Link to="/">Home</Link>
       <h1>Authorisation</h1>
-      <form onSubmit={onSubmit}>
-        <input name="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Email"/>
-        <input name="password" value={password} onChange={e => setPassword(e.target.value)} type="password" placeholder="Password"/>
-        <button type="submit">Submit</button>
-      </form>
-      <Link to="/register" >Register</Link>
+      
+      <Form
+      style = {{
+        maxWidth: "400px",
+        textAlign: "center",
+        display: "block",
+        marginLeft: "auto",
+        marginRight: "auto",
+
+      }}
+      name="basic"
+        initialValues={{
+          remember: true,
+        }}
+      
+      >
+      <Form.Item
+        label="Email"
+        name="email"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your email!',
+          },
+        ]}
+      >
+        <Input prefix={<UserOutlined className="site-form-item-icon" />} value={email} onChange={e => setEmail(e.target.value)} placeholder="Email"/>
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+      >
+        <Input prefix={<LockOutlined className="site-form-item-icon" />} value={password} onChange={e => setPassword(e.target.value)} type="password" placeholder="Password"/>
+      </Form.Item>  
+      <Form.Item style={{width: "100%", textAlign: 'center',}}>
+        <Form.Item style={{float: "left"}} name="remember" valuePropName="checked">
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+        <Link style={{float: "right"}} to="/" >
+          Forgot password
+        </Link>
+      </Form.Item>
+      
+      <Form.Item >
+        <Button style={{width: "100%"}} type="primary" htmlType="submit" onClick={onSubmit}>
+          Log in
+        </Button>
+        Or <Link to="/register" >Register</Link>
+      </Form.Item>
+      </Form> 
+      
     </>
   )
 }
