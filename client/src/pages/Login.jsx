b7cdcad86f75c91dfab7d63857370b319e16c775
Mykhailo Kokadii <m.kokadii@pmpartners.com.ua>
2020-11-10 23:42:41 +0200

    Refactor and fix login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,39 +2,50 @@ import React, { useContext, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { AuthContext } from '../contexts/Auth'
 import 'antd/dist/antd.css';
-import { Form, Input, Button, Checkbox } from 'antd';
+import { Form, Input, Button, Checkbox, Row } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 // import { StepForwardOutlined } from 'antd';
 
+//TODO: 
 
-
-// const layout = {
-//   labelCol: { span: 8 },
-//   wrapperCol: { span: 8 },
-//   textAlign: 'center'
-// };
-// const tailLayout = {
-//   wrapperCol: { offset: 8, span: 8 },
-//   textAlign: 'center'
-// };
-
+//использовать инлайновый css в данном случае плохо, так как
+//у тебя не выравниваються инпуты в один столбик
+//А вот это как раз надо было и использовать))
+const layout = {
+  labelCol: { 
+    lg: { span:8,},
+    md: { span: 8, offset: 2 },
+    sm: { span: 7, offset: 2 },
+  },
+  wrapperCol: { 
+    lg: { span: 6 },
+    md: { span: 7 },
+    sm: { span: 8 },
+  },
+};
+const tailLayout = {
+  wrapperCol: { 
+    lg: { span: 8, offset: 8 },
+    md: { span: 10, offset: 7},
+    sm: { span: 12, offset: 5},
+  },
+};
 
 const LoginPage = () => {
-  const auth = useContext(AuthContext)
+  const { login } = useContext(AuthContext)
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
-  const onSubmit = async event => {
+  const onSubmit = async (values) => {
     try {
-      event.preventDefault()
       const response = await fetch('/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ email, password })
+        body: JSON.stringify(values)
       })
       const result = await response.json()
       if (result) {
-        auth.login(result.token, result.userId)
+        login(values)
       }
     } catch (e) {
       console.error(e)
@@ -45,21 +56,15 @@ const LoginPage = () => {
     <>      
       <Link to="/">Home</Link>
       <h1>Authorisation</h1>
-      
-      <Form
-      style = {{
-        maxWidth: "400px",
-        textAlign: "center",
-        display: "block",
-        marginLeft: "auto",
-        marginRight: "auto",
-
-      }}
-      name="basic"
+      {/* Делай внятные отсупы, что бы было видно уровень вложенности */}
+      <Form 
+        {...layout}
+      name="login" //название должно говорить о чем-то
         initialValues={{
-          remember: true,
+          remember: true, //вот эта штучка пока ни на что не влияет))
         }}
-      
+        //сабмит формы лучше делать не на клике по кнопке а у формы есть специальный метод
+        onFinish={onSubmit}
       >
       <Form.Item
         label="Email"
@@ -71,7 +76,14 @@ const LoginPage = () => {
           },
         ]}
       >
-        <Input prefix={<UserOutlined className="site-form-item-icon" />} value={email} onChange={e => setEmail(e.target.value)} placeholder="Email"/>
+        {/* если пропсов много то лучше их форматировать вот так */}
+        <Input 
+          prefix={<UserOutlined className="site-form-item-icon" />} 
+          value={email} 
+          type="email"
+          onChange={e => setEmail(e.target.value)} 
+          placeholder="Email"
+        />
       </Form.Item>
 
       <Form.Item
@@ -84,19 +96,30 @@ const LoginPage = () => {
           },
         ]}
       >
-        <Input prefix={<LockOutlined className="site-form-item-icon" />} value={password} onChange={e => setPassword(e.target.value)} type="password" placeholder="Password"/>
+        <Input 
+          prefix={<LockOutlined className="site-form-item-icon" />} 
+          value={password} 
+          onChange={e => setPassword(e.target.value)} 
+          type="password" 
+          placeholder="Password"
+        />
       </Form.Item>  
-      <Form.Item style={{width: "100%", textAlign: 'center',}}>
-        <Form.Item style={{float: "left"}} name="remember" valuePropName="checked">
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
-        <Link style={{float: "right"}} to="/" >
-          Forgot password
-        </Link>
+      <Form.Item {...tailLayout}>
+        <Row justify="space-between" align="middle">
+          <Form.Item name="remember" valuePropName="checked">
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+          <Form.Item>
+            <Link  to="/" >
+              Forgot password
+            </Link>
+          </Form.Item>
+        </Row>
       </Form.Item>
       
-      <Form.Item >
-        <Button style={{width: "100%"}} type="primary" htmlType="submit" onClick={onSubmit}>
+      {/* для кнопки попробуй выравнивание сделать сам */}
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
           Log in
         </Button>
         Or <Link to="/register" >Register</Link>
